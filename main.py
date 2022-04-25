8c1f8d2ae6a3852991696b67d73ffe89347d5f93
marven <jeanjacquesmarven@gmail.com>
2022-04-25 17:59:38 -0400

    shopping list

--- main.py
+++ main.py
@@ -5,19 +5,19 @@
 
 
 def salesTax():
-    state = input("Enter the State you're in? (MA,NH,MA) ")
-    if state in ['MA', 'ma' 'massachusett', 'Massachusett']:
+    state = input("Enter the State you're in? (MA,NH,MA): ")
+    if state in ['MA', 'ma', 'massachusett', 'Massachusett']:
         state = 'Massachusett'
         tax = 0.625
         print(state, tax)
         return tax
-    if state in ['HN', 'nh' 'New Hampshire', 'new hampshire']:
+    if state in ['HN', 'nh', 'New Hampshire', 'new hampshire']:
         state = 'New Hampshire'
         tax = 0
         print(state, tax)
         return tax
-    if state in ['ME', 'me' 'Maine', 'maine']:
-        state= 'Maine'
+    if state in ['ME', 'me', 'Maine', 'maine']:
+        state = 'Maine'
         tax = 0.55
         print(state, tax)
         return tax
@@ -25,10 +25,20 @@ def salesTax():
         exit('State Error, We only working with those following state: Massachusett, New Hampshire, Maine')
 
 def product():
+    wicEligibleFood =['apples', 'bananas', 'carrots', 'potatoes','Cheese','Milk','Juice','Bread','Cereal','Yogurt']
+    for i in wicEligibleFood:
+        print(i)
+    clothing = ['jeans', 'shirt', 't-shirt', 'pantys' 'boxer', 'sweater','jacket','coat','socks','shorts','tracksuit']
+    for i in clothing:
+        print(i)
+    everytingElse =['Refrigerator ','cooktop ','Dishwasher ','washer ','dryer','Microwave ','oven']
+    for i in everytingElse:
+        print(i)
     return
 
  #Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     salesTax()
+    product()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
