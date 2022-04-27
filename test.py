9409d509010982885587ea0986186ca863450c0d
mjeanjac4 <jeanjacquesmarven@gmail.com>
2022-04-27 17:55:10 -0400

    Test init

--- test.py
+++ test.py
@@ -4,13 +4,17 @@ import main
 class TestChoppingList(unittest.TestCase):
 
     def test_state(self):
-        result = main.salesTax('ma')
+        result = main.salesTax()
+        if sta
         self.assertEqual(result,('Massachusett', 0.625))
 
     def test_state(self):
         result = main.salesTax('me')
-        self.assertEqual(result,('new hampshire', 0.625))
-        self.assertEqual(result,('maine', 0.55))
+        self.assertEqual(result,('Maine', 0.55))
+
+    def test_state(self):
+        result = main.salesTax()
+        self.assertEqual(result,('New Hampshire', 0))
 
 
 
