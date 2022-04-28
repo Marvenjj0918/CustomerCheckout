9c521502459c6d6146359b96d2ea5d6808df59b6
mjeanjac4 <jeanjacquesmarven@gmail.com>
2022-04-28 13:44:42 -0400

    remove customer input

--- checkoutForTestingPurpusing.py
+++ checkoutForTestingPurpusing.py
@@ -1,55 +1,36 @@
 from pyrecord import Record
 
-def product(tax, Sum):
-    # typeOfGoog = Record.create_type("typeOfGoog","name", "price" )
-    # wicEligibleFood = typeOfGoog.extend_type("wicEligibleFood")
-    # everytingElse = typeOfGoog.extend_type("everytingElse")
-    # clothing = typeOfGoog.extend_type("clothing")
-    # apple = wicEligibleFood("apple", 2.99)
-    # bananas = wicEligibleFood("bananas",4.4)
-    # jeans = clothing("jeans", 25)
-    # shirt = clothing("shirt", 15)
-    # Refrigerator = everytingElse("Refrigerator", 585)
-    # Dishwasher = everytingElse("Dishwasher", 260)
-    # print(apple, bananas)
-    # print(jeans, shirt)
-    # print(Refrigerator, Dishwasher)
-    # numberOfItem = int(input("How many item are you buying?"))
-    # j = 0
-    # newShoppingList=[]
-    # while j != numberOfItem:
-    #     shoppinglist = input("Please choose a item in the lis above:")
-    #     if shoppinglist == "apple":
-    #         price = apple.price
-    #         print("Apple ", "$", price)
-    #     elif shoppinglist == "bananas":
-    #         price = bananas.price
-    #         print("bananas", "$", price)
-    #     elif shoppinglist == "jeans":
-    #         price = jeans.price
-    #         print("jeans", "$", price)
-    #     elif shoppinglist == "shirt":
-    #         price = shirt.price
-    #         print("shirt", "$", price)
-    #     elif shoppinglist == "Refrigerator":
-    #         price = Refrigerator.price
-    #         print("Refrigerator", "$", price)
-    #     elif shoppinglist == "Dishwasher":
-    #         price = Dishwasher.price
-    #         print("Dishwasher", "$", price)
-    #     else:
-    #         print("not in the list")
-    #         price = 0
-    #     newShoppingList.append(price)
-    #     numberOfItem = numberOfItem-1
-    # Sum = sum(newShoppingList)
-    #print("Total before tax is: ","$",Sum)
-    totals = (Sum * tax) + Sum
-    #print("Sales tax is:",tax)
-    print(totals)
+def product(state):
+    typeOfGoog = Record.create_type("typeOfGoog","name", "price" )
+    wicEligibleFood = typeOfGoog.extend_type("wicEligibleFood")
+    everytingElse = typeOfGoog.extend_type("everytingElse")
+    clothing = typeOfGoog.extend_type("clothing")
+    apple = wicEligibleFood("apple", 2.99)
+    bananas = wicEligibleFood("bananas", 4.5)
+    jeans = clothing("jeans", 25)
+    shirt = clothing("shirt", 15)
+    Refrigerator = everytingElse("Refrigerator", 585)
+    Dishwasher = everytingElse("Dishwasher", 260)
+    newShoppingList = apple.price + bananas.price + jeans.price + shirt.price + Refrigerator.price + Dishwasher.price
+    #newShoppingList.append("price")
+    print(newShoppingList)
 
+    if state in ['MA', 'ma', 'massachusett', 'Massachusett']:
+        tax = 0.625
+        totals = (newShoppingList * tax) + newShoppingList
+        print(totals)
+        return totals
+    elif state in ['NH', 'nh', 'New Hampshire', 'new hampshire']:
+        tax = 0
+        totals = (newShoppingList * tax) + newShoppingList
+        print(totals)
 
+        return tax
+    elif state in ['ME', 'me', 'Maine', 'maine']:
+        tax = 0.55
+        totals = (newShoppingList * tax) + newShoppingList
+        print(totals)
 
-
-def total(sate):
-    return
+        return tax
+    else:
+        exit('State Error, We only working with those following state: Massachusett, New Hampshire, Maine')
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import checkout
 import checkoutForTestingPurpusing
 
 if __name__ == '__main__':
-    checkout.salesTax()
-    checkout.product()
-    checkoutForTestingPurpusing.product(0.5, 245)
+   # checkout.product()
+    checkoutForTestingPurpusing.product("nh")
 
